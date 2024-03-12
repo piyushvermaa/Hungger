fd27a7966a8dcc7cc1d90a746d66f266f666be80
piyushvermaa <piyush.verma_cs22@gla.ac.in>
2024-03-13 00:43:13 +0530

    profile card extended

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -18,6 +18,41 @@ export const UserProfileCard = () => {
             <div id="button">
                 <button id="btn" onClick={handleFollow}>Follow <i className="fas fa-user-plus"></i></button>
             </div>
+            <div className="ds-info">
+          <div className="ds pens">
+            <h6 title="Number of pens created by the user">Pens <i className="fas fa-edit"></i></h6>
+            <p>29</p>
+          </div>
+          <div className="ds projects">
+            <h6 title="Number of projects created by the user">Projects <i className="fas fa-project-diagram"></i></h6>
+            <p>0</p>
+          </div>
+          <div className="ds posts">
+            <h6 title="Number of posts">Posts <i className="fas fa-comments"></i></h6>
+            <p>0</p>
+          </div>
+        </div>
+        <div className="ds-skill">
+          <h6>Skill <i className="fa fa-code" aria-hidden="true"></i></h6>
+          <div className="skill html">
+            <h6><i className="fab fa-html5"></i> React </h6>
+            <div className="bar bar-html">
+              <p>95%</p>
+            </div>
+          </div>
+          <div className="skill css">
+            <h6><i className="fab fa-css3-alt"></i> Tailwind </h6>
+            <div className="bar bar-css">
+              <p>90%</p>
+            </div>
+          </div>
+          <div className="skill javascript">
+            <h6><i className="fab fa-js"></i> JavaScript </h6>
+            <div className="bar bar-js">
+              <p>75%</p>
+            </div>
+          </div>
+        </div>  
         </div>
     );
 };
